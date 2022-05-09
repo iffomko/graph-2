66bd729e6e3a7a5b26c8a75eff2e85d68b6b77d6
Cromulent74 <ilya.fomko03@mail.ru>
2022-05-09 12:03:58 +0500

    update main func

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <fstream>
 
+#include "cpp/readMatrix/readMatrix.h"
+#include "cpp/findIsolatedTops/findIsolatedTops.h"
+
 int main() {
     std::ifstream fin("C:\\Users\\Iluha\\Documents\\github_repos\\graph-2\\input.txt");
 
-    int n, count = 0;
-    int** matrix;
-    std::string result = "";
+    int n, count;
+    int** matrix = readMatrix(fin, n);
+    std::string result;
 
-    
+    if (matrix != nullptr)
+        findIsolatedTops(count, result, matrix, n);
+    else
+        return 1;
 
     if (count == 0)
         std::cout << "Graph don't have a isolated tops..." << std::endl;
     else {
-        std::cout << "Count of isolated tops: " << count << ", numbers of " << result << std::endl;
+        std::cout << "Count of isolated tops: " << count << ", numbers: " << result << std::endl;
     }
 
     return 0;
